6a60167045f734ac18bdf99dbf111ca7b29b49de
akbarssha <akbarprd28@gmail.com>
2026-03-16 13:34:29 +0530

    fix route

--- frontend/front/src/components/api/axios.js
+++ frontend/front/src/components/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://assa-bridal-backend.onrender.com/api",
+  baseURL: "https://assa-bridal-backend.onrender.com",
 });
 
 // attach token automatically
